5a61a18291ebeca1aa983f26c76d38e54d5d1881
Rahul Singh Aswal <aswalrahul576@gmail.com>
2025-05-14 00:42:08 +0530

    chore: add basic redux setup with toolkit

---
+++ src/Redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import authSliceReducer from './Slices/AuthSlice.js';
+const store = configureStore({
+  reducer: { auth: authSliceReducer },
+  devTools: true,
+});
+
+export default store;
