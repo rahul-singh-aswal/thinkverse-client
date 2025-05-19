ba05241e5c1c542e060a17cf29a204687c7b11ef
Rahul Singh Aswal <aswalrahul576@gmail.com>
2025-05-20 01:21:17 +0530

    feat: add sign up flow

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import React from 'react';
 import HomePage from './Pages/HomePage';
 import About from './Pages/About';
 import NotFound from './Pages/NotFound';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
